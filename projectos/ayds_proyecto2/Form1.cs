27f46eaa96c10f6f044fab7189d07d07ed7d9604
sopladoman <166187700+alexsilvauam@users.noreply.github.com>
2024-10-30 08:45:08 -0600

    Add files via upload

---
+++ projectos/ayds_proyecto2/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ayds_proyecto2
+{
+    public partial class Form : MetroFramework.Forms.MetroForm
+    {
+        public Form()
+        {
+            InitializeComponent();
+            this.StyleManager = metroStyleManager1;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroLabel1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroLabel1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroLabel3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblPrecio_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroTextBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroTile1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolTip1_Popup(object sender, PopupEventArgs e)
+        {
+
+        }
+
+        private void metroToggle1_CheckedChanged(object sender, EventArgs e)
+        {
+            if(metroToggle1.Checked)
+            {
+                metroStyleManager1.Theme= MetroFramework.MetroThemeStyle.Dark;
+            }
+            else
+            {
+                metroStyleManager1.Theme = MetroFramework.MetroThemeStyle.Light;
+            }    
+
+        }
+    }
+}
